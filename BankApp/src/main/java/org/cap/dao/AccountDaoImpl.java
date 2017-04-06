7a198f0f1c4f87c07617faab40cd08d02fcb9b1f
ashshanm <ashshanm@DIN76000170A.corp.capgemini.com>
2017-04-06 17:49:57 +0530

    branch1 changed

--- BankApp/src/main/java/org/cap/dao/AccountDaoImpl.java
+++ BankApp/src/main/java/org/cap/dao/AccountDaoImpl.java
@@ -7,13 +7,12 @@ public class AccountDaoImpl implements AccountDao{
 
 	@Override
 	public boolean createAccount(Account account) {
-		// TODO Auto-generated method stub
+		System.out.println("Branch2");
 		return false;
 	}
 
 	@Override
 	public Account findAccountById(int accountNo) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
